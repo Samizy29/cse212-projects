040a8dc2be3ac75e42ea9ef2209dfb750ec999aa
Samizy29 <onisamuel73@gmail.com>
2026-03-03 15:14:55 +0100

    Arrays.cs assignment do passed 7

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,8 +13,16 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        double[] result = new double[length];
+
+for (int i = 0; i < length; i++)
+{
+    result[i] = number * (i + 1);
+}
+
+return result;
     }
+     // replace this return statement with your own
 
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
@@ -29,5 +37,13 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        int splitIndex = data.Count - amount;
+
+List<int> lastPart = data.GetRange(splitIndex, amount);
+List<int> firstPart = data.GetRange(0, splitIndex);
+
+data.Clear();
+data.AddRange(lastPart);
+data.AddRange(firstPart);
     }
 }
